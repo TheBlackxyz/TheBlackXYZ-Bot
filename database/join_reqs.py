87c045c9f8eebc34f1c918e4fd5d5750c192643f
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-16 14:23:36 +0530

    Add files via upload

---
+++ database/join_reqs.py
@@ -0,0 +1,57 @@
+# Credit @TheBlackXYZ.
+# Please Don't remove credit.
+# TheBlackXYZBotz Forever !
+# Thanks You For Help Us In This Amazing Creativity 
+# Thanks You For Giving Me Credit @TheBlackXYZBotz
+# For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
+# Please Love & Support 💗💗🙏
+
+import motor.motor_asyncio
+from info import AUTH_CHANNEL, DATABASE_URI
+
+class JoinReqs:
+
+    def __init__(self):
+        if DATABASE_URI:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
+            self.db = self.client["JoinReqs"]
+            self.col = self.db[str(AUTH_CHANNEL)]
+        else:
+            self.client = None
+            self.db = None
+            self.col = None
+
+    def isActive(self):
+        if self.client is not None:
+            return True
+        else:
+            return False
+
+    async def add_user(self, user_id, first_name, username, date):
+        try:
+            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+        except:
+            pass
+
+    async def get_user(self, user_id):
+        return await self.col.find_one({"user_id": int(user_id)})
+
+    async def get_all_users(self):
+        return await self.col.find().to_list(None)
+
+    async def delete_user(self, user_id):
+        await self.col.delete_one({"user_id": int(user_id)})
+
+    async def delete_all_users(self):
+        await self.col.delete_many({})
+
+    async def get_all_users_count(self):
+        return await self.col.count_documents({})
+
+# Credit @TheBlackXYZ.
+# Please Don't remove credit.
+# TheBlackXYZBotz Forever !
+# Thanks You For Help Us In This Amazing Creativity 
+# Thanks You For Giving Me Credit @TheBlackXYZBotz
+# For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
+# Please Love & Support 💗💗🙏
