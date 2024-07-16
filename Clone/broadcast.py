76ed31ca90d5742afcd658912d5cf81248077b9b
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-16 16:29:39 +0530

    Create broadcast.py

---
+++ Clone/broadcast.py
@@ -0,0 +1,78 @@
+# Credit @TheBlackXYZ.
+# Please Don't remove credit.
+# TheBlackXYZBotz Forever !
+# Thanks You For Help Us In This Amazing Creativity 
+# Thanks You For Giving Me Credit @TheBlackXYZBotz
+# For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
+# Please Love & Support 💗💗🙏
+
+import datetime, time, asyncio
+from pyrogram import Client, filters
+from database.users_chats_db import db
+from pyrogram.errors import *
+from CloneTechVJ.database.clone_bot_userdb import clonedb
+        
+@Client.on_message(filters.command("broadcast"))
+async def pm_broadcast(bot, message):
+    me = await bot.get_me()
+    owner = await db.get_bot(me.id)
+    if owner["user_id"] != message.from_user.id:
+        return 
+    b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
+    try:
+        users = await clonedb.get_all_users(me.id)
+        sts = await message.reply_text('Broadcasting your messages...')
+        start_time = time.time()
+        total_users = await clonedb.total_users_count(me.id)
+        done = 0
+        blocked = 0
+        deleted = 0
+        failed = 0
+        success = 0
+        async for user in users:
+            if 'user_id' in user:
+                pti, sh = await broadcast_messages(me.id, int(user['user_id']), b_msg)
+                if pti:
+                    success += 1
+                elif pti == False:
+                    if sh == "Blocked":
+                        blocked += 1
+                    elif sh == "Deleted":
+                        deleted += 1
+                    elif sh == "Error":
+                        failed += 1
+                done += 1
+                if not done % 20:
+                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            else:
+                # Handle the case where 'id' key is missing in the user dictionary 
+                done += 1
+                failed += 1
+                if not done % 20:
+                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    
+        time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+        await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    except Exception as e:
+        print(f"error: {e}")
+
+async def broadcast_messages(bot_id, user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(bot_id, user_id, message)
+    except InputUserDeactivated:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Deleted"
+    except UserIsBlocked:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Error"
+    except Exception as e:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Error"
+      
