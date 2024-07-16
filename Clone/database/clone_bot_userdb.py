42d7a9c374ee1f4b912ad4388ef3dd7ff9a737eb
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-16 16:28:32 +0530

    Create clone_bot_userdb.py

---
+++ Clone/database/clone_bot_userdb.py
@@ -0,0 +1,30 @@
+import motor.motor_asyncio
+from info import CLONE_DATABASE_URI, DATABASE_NAME
+
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+
+    async def add_user(self, bot_id, user_id):
+        user = {'user_id': int(user_id)}
+        await self.db[str(bot_id)].insert_one(user)
+    
+    async def is_user_exist(self, bot_id, id):
+        user = await self.db[str(bot_id)].find_one({'user_id': int(id)})
+        return bool(user)
+    
+    async def total_users_count(self, bot_id):
+        count = await self.db[str(bot_id)].count_documents({})
+        return count
+
+    async def get_all_users(self, bot_id):
+        return self.db[str(bot_id)].find({})
+
+    async def delete_user(self, bot_id, user_id):
+        await self.db[str(bot_id)].delete_many({'user_id': int(user_id)})
+
+
+clonedb = Database(CLONE_DATABASE_URI, DATABASE_NAME)
+  
