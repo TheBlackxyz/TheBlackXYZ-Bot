e44727f6ad62cec9ab044afc36ac80dda1e1d6aa
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-16 14:22:34 +0530

    Add files via upload

---
+++ server/exceptions.py
@@ -0,0 +1,13 @@
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
+
+# Credit @TheBlackXYZ.
+# Please Don't remove credit.
+# TheBlackXYZBotz Forever !
+# Thanks You For Help Us In This Amazing Creativity 
+# Thanks You For Giving Me Credit @TheBlackXYZBotz
+# For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
+# Please Love & Support 💗💗🙏
