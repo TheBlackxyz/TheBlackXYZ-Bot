ccb85f19cc784808942ac1be495141e5d8ec0245
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-23 20:44:36 +0530

    Update tts.py

--- plugins/Extra/tts.py
+++ plugins/Extra/tts.py
@@ -24,14 +24,14 @@ def convert(text):
 
 @Client.on_message(filters.command("tts"))
 async def text_to_speech(bot, message: Message):
-    vj = await bot.ask(chat_id = message.from_user.id, text = "Now send me your text.")
-    if vj.text:
-        m = await vj.reply_text("Processing")
-        text = vj.text
+    TheBlackXYZBotz = await bot.ask(chat_id = message.from_user.id, text = "Now send me your text.")
+    if TheBlackXYZBotz.text:
+        m = await TheBlackXYZBotz.reply_text("Processing")
+        text = TheBlackXYZBotz.text
         try:
             loop = get_running_loop()
             audio = await loop.run_in_executor(None, convert, text)
-            await vj.reply_audio(audio)
+            await TheBlackXYZBotz.reply_audio(audio)
             await m.delete()
             audio.close()
         except Exception as e:
@@ -39,7 +39,7 @@ async def text_to_speech(bot, message: Message):
             e = traceback.format_exc()
             print(e)
     else:
-        await vj.reply_text("Send me only text Buddy.")
+        await TheBlackXYZBotz.reply_text("Send me only text Buddy.")
 
 
 
