97417d2f690a8968eed0bbcad5fcc219cd45c552
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-23 20:59:41 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -18,20 +18,20 @@ API = "https://apis.xditya.me/lyrics?song="
 
 @Client.on_message(filters.text & filters.command(["lyrics"]))
 async def sng(bot, message):
-    vj = await bot.ask(chat_id=message.from_user.id, text="Now send me your song name.")
-    if vj.text:
-        mee = await vj.reply_text("`Searching 🔎`")
-        song = vj.text
+    TheBlackXYZBotz = await bot.ask(chat_id=message.from_user.id, text="Now send me your song name.")
+    if TheBlackXYZBotz.text:
+        mee = await TheBlackXYZBotz.reply_text("`Searching 🔎`")
+        song = TheBlackXYZBotz.text
         chat_id = message.from_user.id
         rpl = lyrics(song)
         await mee.delete()
         try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/vj_botz")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/TheBlackXYZBotz")]]))
         except Exception as e:                            
-            await vj.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/TheBlackXYZ")]]))
+            await TheBlackXYZBotz.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/TheBlackXYZ")]]))
     else:
-        await vj.reply_text("Send me only text Buddy.")
+        await TheBlackXYZBotz.reply_text("Send me only text Sweetie 💛.")
 
 
 def search(song):
